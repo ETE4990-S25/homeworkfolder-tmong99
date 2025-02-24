cc8cd836db7f993ae4e230103e8b69c1673cf8e4
Matthew Tong <tmong99@gmail.com>
2025-02-23 20:48:05 -0800

    created data_classes.py

--- Homework/Homework 3/data_classes.py
+++ Homework/Homework 3/data_classes.py
@@ -1,1 +1,25 @@
-class Person (object)
+import json
+
+class Person (object):
+    def __init__(self, name, age, email):
+        self.name = name
+        self.age = age
+        self.email = email
+
+class Student(Person):
+    def __init__(self, name, age, email, studentID):
+        super().__init__(self, name, age, email)
+        self.studentID = studentID
+
+    def write_dict(self):
+        return{
+            "name": self.name,
+            "age": self.age, 
+            "email": self.email,
+            "student ID": self.studentID
+
+        }
+
+    def save_to_json(self, file_name):
+        with open(file_name, 'w') as file:
+            json.dump(self.write_dict(), file, indent = 4)
